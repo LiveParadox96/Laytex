13a767f50bb9791cb0c0590183efd0a72245e4f0
Maksim El'kin <LiveParadox@vk.com>
2023-04-22 20:27:19 +0300

    prohibited data entry in the feedback in the absence of a checkmark confirming the processing of personal data. additionally added deleting data when unchecking the checkbox

--- feedback.js
+++ feedback.js
@@ -1,10 +1,25 @@
 const checkbox = document.querySelector('input[type="checkbox"]');
 const feedback = document.querySelector('.feedback');
+const nameInput = document.querySelector('.name-data')
+const phoneInput = document.querySelector('.phone-data')
+nameInput.setAttribute('disabled', true)
+phoneInput.setAttribute('disabled', true)
+
 
 checkbox.addEventListener('click', function() {
   if (checkbox.checked) {
     feedback.classList.add('active');
-  } else {
+    nameInput.removeAttribute('disabled')
+    phoneInput.removeAttribute('disabled')
+
+} else {
     feedback.classList.remove('active');
+    nameInput.setAttribute('disabled', true);
+    phoneInput.setAttribute('disabled', true);
   }
+
 });
+
+// if(!checkbox.checked){
+//     return name.setAttribute('maxlenght', 0)
+// }
